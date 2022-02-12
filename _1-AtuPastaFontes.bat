27318c8bb2057a0a589be382436d2a786f7a1181
JMGrenhas <jmpmgrenhas@gmail.com>
2022-02-12 13:37:47 +0000

    -

--- _1-AtuPastaFontes.bat
+++ _1-AtuPastaFontes.bat
@@ -3,7 +3,7 @@ copy C:\MEI\_PE\Disserta‡Æo_20-21\ANB-Prototype\Lib\myLists.py NameSearch\
 copy C:\MEI\_PE\Disserta‡Æo_20-21\ANB-Prototype\Lib\myStrings.py NameSearch\
 copy C:\MEI\_PE\Disserta‡Æo_20-21\ANB-Prototype\myNames.py NameSearch\
 
-@echo Fazer as seguintes adapta‡äes:
+@echo Fazer as seguintes adaptacoes:
 @echo Em myNames.py:
 @echo   "from Lib.myLists" === "from .myLists"
 @echo   "from Lib.myStrings" === "from .myStrings"
